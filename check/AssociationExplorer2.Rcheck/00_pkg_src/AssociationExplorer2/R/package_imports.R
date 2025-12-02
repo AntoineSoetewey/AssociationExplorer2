975c77c4c2ab33b2d915e87d33dd458d482c24ba
Antoine Soetewey <ant.soetewey@gmail.com>
2025-12-02 10:25:50 +0100

    Add package structure for AssociationExplorer2

---
+++ check/AssociationExplorer2.Rcheck/00_pkg_src/AssociationExplorer2/R/package_imports.R
@@ -0,0 +1,25 @@
+#' Internal package imports
+#'
+#' These import statements ensure that the packages listed in the
+#' Imports field of DESCRIPTION are properly imported into the
+#' package namespace, as required for CRAN.
+#'
+#' @keywords internal
+#' @name package_imports
+#'
+#' @import shiny
+#' @import visNetwork
+#' @import ggplot2
+#' @import plotly
+#' @import dplyr
+#' @import tidyr
+#' @import readr
+#' @import readxl
+#' @import stringr
+#' @import purrr
+#' @import tibble
+#' @import forcats
+#' @import reactable
+#' @import scales
+#' @import igraph
+NULL
